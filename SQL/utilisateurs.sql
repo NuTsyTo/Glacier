8cc9539f8367fe10a65ab86847f017cb171f87b1
NuTsyTo <135851698+NuTsyTo@users.noreply.github.com>
2023-08-08 11:08:06 +0200

    Delete SQL directory

--- SQL/utilisateurs.sql
+++
@@ -1,5 +0,0 @@
-insert into utilisateurs (id, email, password) values (1, 'dbatey0@nsw.gov.au', 'jV0');
-insert into utilisateurs (id, email, password) values (2, 'tdevaux1@typepad.com', 'lC6');
-insert into utilisateurs (id, email, password) values (3, 'jboreland2@usatoday.com', 'jG8');
-insert into utilisateurs (id, email, password) values (4, 'apigot3@reverbnation.com', 'hK9');
-insert into utilisateurs (id, email, password) values (5, 'wstrephan4@mac.com', 'jL1G');
